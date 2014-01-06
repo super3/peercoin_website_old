7964f09a47fef5876fdc0fabb14c3629e278e88c
Art <cipherwraith666@gmail.com>
2014-01-06 13:01:42 +0800

    Changed indentation from tabs to 2 spaces.

--- footer.php
+++ footer.php
@@ -1,43 +1,43 @@
-	<!-- FOOTER -->
-	<footer class="footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-3">
-					<h5 style="color: white;">Info</h5>
-					<a href="bitaddress.html">Wallet Generator (Beta!)</a><br>
-					<a href="https://github.com/ppcoin/ppcoin">Source Code (Github)</a><br>
-					<a href="https://github.com/ppcoin/ppcoin/wiki">Wiki</a><br>
-					<a href="http://www.peercointalk.org/">Forum</a><br>
-					<a href="http://peercoinmyths.com/">Myths</a>
-				</div>
-				<div class="col-lg-3">
-					<h5 style="color: white;">Exchanges</h5>
-					<a href="https://vircurex.com/welcome/index?alt=ppc&amp;base=btc&amp;locale=en">Vircurex</a><br>
-					<a href="https://btc-e.com/exchange/ppc_btc/">BTC-E</a><br>
-					<a href="https://mcxnow.com/exchange/PPC">MCX Now</a><br>
-					<a href="https://coinmkt.com/">CoinMkt</a><br>
-					<a href=" https://crypto-trade.com/trade/ppc_btc">Crypto-Trade</a>
-				</div>
+  <!-- FOOTER -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3">
+          <h5 style="color: white;">Info</h5>
+          <a href="bitaddress.html">Wallet Generator (Beta!)</a><br>
+          <a href="https://github.com/ppcoin/ppcoin">Source Code (Github)</a><br>
+          <a href="https://github.com/ppcoin/ppcoin/wiki">Wiki</a><br>
+          <a href="http://www.peercointalk.org/">Forum</a><br>
+          <a href="http://peercoinmyths.com/">Myths</a>
+        </div>
+        <div class="col-lg-3">
+          <h5 style="color: white;">Exchanges</h5>
+          <a href="https://vircurex.com/welcome/index?alt=ppc&amp;base=btc&amp;locale=en">Vircurex</a><br>
+          <a href="https://btc-e.com/exchange/ppc_btc/">BTC-E</a><br>
+          <a href="https://mcxnow.com/exchange/PPC">MCX Now</a><br>
+          <a href="https://coinmkt.com/">CoinMkt</a><br>
+          <a href=" https://crypto-trade.com/trade/ppc_btc">Crypto-Trade</a>
+        </div>
 
-				<div class="col-lg-3">
-					<h5 style="color: white;">More Exchanges</h5>
-					<a href="http://bter.com/trade/ppc_btc">BTER Exchange</a><br>
-					<a href="http://cryptonit.net/exchange/btc/ppc">Cryptonit</a><br>
-					<a href="https://www.cryptsy.com/markets/view/28">Cryptsy</a><br>
-					<a href="https://www.coins-e.com/exchange/PPC_BTC/">Coins-e</a><br>
-					<a href="https://www.vaultofsatoshi.com/">Vault of Satoshi</a>
-				</div>
+        <div class="col-lg-3">
+          <h5 style="color: white;">More Exchanges</h5>
+          <a href="http://bter.com/trade/ppc_btc">BTER Exchange</a><br>
+          <a href="http://cryptonit.net/exchange/btc/ppc">Cryptonit</a><br>
+          <a href="https://www.cryptsy.com/markets/view/28">Cryptsy</a><br>
+          <a href="https://www.coins-e.com/exchange/PPC_BTC/">Coins-e</a><br>
+          <a href="https://www.vaultofsatoshi.com/">Vault of Satoshi</a>
+        </div>
 
-				<div class="col-lg-3">
-					<h5 style="color: white;">Mining</h5>
-					<a href="https://www.coinotron.com/">Coinotron</a><br>
-					<a href="https://ppcoin.d7.lt/">D7 Pool</a><br>
-					<a href="http://ppc.fixx.ru/index">Fixx</a>
-				</div>
-			</div><!-- /row -->
+        <div class="col-lg-3">
+          <h5 style="color: white;">Mining</h5>
+          <a href="https://www.coinotron.com/">Coinotron</a><br>
+          <a href="https://ppcoin.d7.lt/">D7 Pool</a><br>
+          <a href="http://ppc.fixx.ru/index">Fixx</a>
+        </div>
+      </div><!-- /row -->
 
-		</div><!-- /container -->
-	</footer>
+    </div><!-- /container -->
+  </footer>
 
 
 
@@ -47,51 +47,51 @@
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/retina.js"></script>
-	<script src="assets/js/jquery.themepunch.revolution.min.js"></script>
-	<script src="assets/js/jquery.themepunch.plugins.min.js"></script>
-	<script src="assets/js/peercoin.js"></script>
-	<script>
-	$(document).ready(function() {
-	//Slideshow
-	$('.banner').revolution({
-	delay:9000,
-	startwidth:1040,
-	startheight:463,
-	onHoverStop:"off",						// Stop Banner Timet at Hover on Slide on/off
+  <script src="assets/js/jquery.themepunch.revolution.min.js"></script>
+  <script src="assets/js/jquery.themepunch.plugins.min.js"></script>
+  <script src="assets/js/peercoin.js"></script>
+  <script>
+  $(document).ready(function() {
+  //Slideshow
+  $('.banner').revolution({
+  delay:9000,
+  startwidth:1040,
+  startheight:463,
+  onHoverStop:"off",            // Stop Banner Timet at Hover on Slide on/off
 
-	thumbWidth:100,							// Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
-	thumbHeight:50,
-	thumbAmount:3,
+  thumbWidth:100,              // Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
+  thumbHeight:50,
+  thumbAmount:3,
 
-	hideThumbs:0,
-	navigationType:"bullet",				// bullet, thumb, none
-	navigationArrows:"none",				// nexttobullets, solo (old name verticalcentered), none
+  hideThumbs:0,
+  navigationType:"bullet",        // bullet, thumb, none
+  navigationArrows:"none",        // nexttobullets, solo (old name verticalcentered), none
 
-	navigationStyle:"round-old",			// round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
-
-
-	navigationHAlign:"center",				// Vertical Align top,center,bottom
-	navigationVAlign:"bottom",				// Horizontal Align left,center,right
-	navigationHOffset:-419,
-	navigationVOffset:72,
-
-	touchenabled:"on",						// Enable Swipe Function : on/off
-
-	stopAtSlide:-1,							// Stop Timer if Slide "x" has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
-	stopAfterLoops:-1,						// Stop Timer if All slides has been played "x" times. IT will stop at THe slide which is defined via stopAtSlide:x, if set to -1 slide never stop automatic
-
-	hideCaptionAtLimit:0,					// It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
-	hideAllCaptionAtLilmit:0,				// Hide all The Captions if Width of Browser is less then this value
-	hideSliderAtLimit:0,					// Hide the whole slider, and stop also functions if Width of Browser is less than this value
+  navigationStyle:"round-old",      // round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
 
 
-	fullWidth:"on",
+  navigationHAlign:"center",        // Vertical Align top,center,bottom
+  navigationVAlign:"bottom",        // Horizontal Align left,center,right
+  navigationHOffset:-419,
+  navigationVOffset:72,
 
-	shadow:0								//0 = no Shadow, 1,2,3 = 3 Different Art of Shadows -  (No Shadow in Fullwidth Version !)
-	})
+  touchenabled:"on",            // Enable Swipe Function : on/off
 
-	});
-	</script>
+  stopAtSlide:-1,              // Stop Timer if Slide "x" has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
+  stopAfterLoops:-1,            // Stop Timer if All slides has been played "x" times. IT will stop at THe slide which is defined via stopAtSlide:x, if set to -1 slide never stop automatic
+
+  hideCaptionAtLimit:0,          // It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
+  hideAllCaptionAtLilmit:0,        // Hide all The Captions if Width of Browser is less then this value
+  hideSliderAtLimit:0,          // Hide the whole slider, and stop also functions if Width of Browser is less than this value
+
+
+  fullWidth:"on",
+
+  shadow:0                //0 = no Shadow, 1,2,3 = 3 Different Art of Shadows -  (No Shadow in Fullwidth Version !)
+  })
+
+  });
+  </script>
 
 
 
