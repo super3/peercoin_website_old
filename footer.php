5430eb03ff7029b64bcdc07f77269bc9ff495289
mrbickle <acintasv@gmail.com>
2014-01-20 21:42:26 +0100

    Added Ecoining Pool

--- footer.php
+++ footer.php
@@ -96,6 +96,7 @@
 				<div class="col-lg-3">
 					<h2>Mining</h2>
 					<ul>
+						<li><a href="http://peercoin.ecoining.com/">Ecoining Pool</a></li>
 						<li><a href="https://www.coinotron.com/">Coinotron</a></li>
 						<li><a href="https://ppcoin.d7.lt/">D7 Pool</a></li>
 						<li><a href="http://ppc.fixx.ru/index">Fixx</a></li>
