84b734e4f7973ca0564a71e28f71e28afda94c1a
iheartcryptocoin <iheartcryptocoin@outlook.com>
2014-03-12 22:38:46 -0400

    Marketing Materials +sec
    -Added index files to all subfolders for security
    -uploaded business cards I created plus our standard coin logo source
    pngs
    -Created marketing materials page to display above materials, more to
    follow as materials are created

---
+++ marketing_materials.php
@@ -0,0 +1,22 @@
+    <?php include ('header.php'); ?>
+
+    <div class="container content">
+      <h1>Peercoin Marketing Materials</h1>
+       <div class="row">
+         <h2> Coin Logos</h2>
+		<div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://peercoin.net/assets/marketing/coin-icon_shadow.png" /></div>
+		<div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://peercoin.net/assets/marketing/coin-icon_shadow2_cropped_edges.png" /></div>
+		<div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://peercoin.net/assets/marketing/Icon-NoShadow.jpg" /></div>
+		<div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://peercoin.net/assets/marketing/Icon-NoShadow2_cropped_edges.png" /></div>
+	</div><!--end row-->
+    <div class="row">
+             <h2>Business Cards</h2>
+  <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://peercoin.net/assets/marketing/PeerCoin_Card_Black.png" /></div>
+  <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://peercoin.net/assets/marketing/PeerCoin_Card_Dark.png" /></div>
+  <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://peercoin.net/assets/marketing/PeerCoin_Card_Green.png" /></div>
+  <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://peercoin.net/assets/marketing/PeerCoin_Card_light.png" /></div>
+  <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="http://peercoin.net/assets/marketing/PeerCoin_Card_White.png" /></div>
+  <div class="col-md-3 col-sm-4 col-xs-6"><a href="http://peercoin.net/assets/marketing/ppc_biz_cards_set.zip">Download all Business Cards(ZIP)</a></div>
+      </div><!--end row-->
+
+    <?php include ('footer.php'); ?>
